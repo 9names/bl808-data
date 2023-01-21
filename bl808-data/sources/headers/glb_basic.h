4f8041a4894970b42a2987b3fb81aec8b008af11
9names <60134748+9names@users.noreply.github.com>
2023-01-21 19:41:20 +1100

    Add entry with non-decimal hex offset to test header

--- bl808-data/sources/headers/glb_basic.h
+++ bl808-data/sources/headers/glb_basic.h
@@ -19,6 +19,62 @@ struct glb_reg {
         } BF;
         uint32_t WORD;
     } core_cfg16;
+
+    /* 0x8C4 : gpio_cfg0 */
+    union {
+        struct {
+            uint32_t reg_gpio_0_ie           : 1; /* [    0],        r/w,        0x0 */
+            uint32_t reg_gpio_0_smt          : 1; /* [    1],        r/w,        0x1 */
+            uint32_t reg_gpio_0_drv          : 2; /* [ 3: 2],        r/w,        0x0 */
+            uint32_t reg_gpio_0_pu           : 1; /* [    4],        r/w,        0x0 */
+            uint32_t reg_gpio_0_pd           : 1; /* [    5],        r/w,        0x0 */
+            uint32_t reg_gpio_0_oe           : 1; /* [    6],        r/w,        0x0 */
+            uint32_t reserved_7              : 1; /* [    7],       rsvd,        0x0 */
+            uint32_t reg_gpio_0_func_sel     : 5; /* [12: 8],        r/w,        0xb */
+            uint32_t reserved_13_15          : 3; /* [15:13],       rsvd,        0x0 */
+            uint32_t reg_gpio_0_int_mode_set : 4; /* [19:16],        r/w,        0x0 */
+            uint32_t reg_gpio_0_int_clr      : 1; /* [   20],        r/w,        0x0 */
+            uint32_t gpio_0_int_stat         : 1; /* [   21],          r,        0x0 */
+            uint32_t reg_gpio_0_int_mask     : 1; /* [   22],        r/w,        0x1 */
+            uint32_t reserved_23             : 1; /* [   23],       rsvd,        0x0 */
+            uint32_t reg_gpio_0_o            : 1; /* [   24],        r/w,        0x0 */
+            uint32_t reg_gpio_0_set          : 1; /* [   25],        w1p,        0x0 */
+            uint32_t reg_gpio_0_clr          : 1; /* [   26],        w1p,        0x0 */
+            uint32_t reserved_27             : 1; /* [   27],       rsvd,        0x0 */
+            uint32_t reg_gpio_0_i            : 1; /* [   28],          r,        0x0 */
+            uint32_t reserved_29             : 1; /* [   29],       rsvd,        0x0 */
+            uint32_t reg_gpio_0_mode         : 2; /* [31:30],        r/w,        0x0 */
+        } BF;
+        uint32_t WORD;
+    } gpio_cfg0;
+
+    /* 0x900 : gpio_cfg15 */
+    union {
+        struct {
+            uint32_t reg_gpio_15_ie           : 1; /* [    0],        r/w,        0x0 */
+            uint32_t reg_gpio_15_smt          : 1; /* [    1],        r/w,        0x1 */
+            uint32_t reg_gpio_15_drv          : 2; /* [ 3: 2],        r/w,        0x0 */
+            uint32_t reg_gpio_15_pu           : 1; /* [    4],        r/w,        0x0 */
+            uint32_t reg_gpio_15_pd           : 1; /* [    5],        r/w,        0x0 */
+            uint32_t reg_gpio_15_oe           : 1; /* [    6],        r/w,        0x0 */
+            uint32_t reserved_7               : 1; /* [    7],       rsvd,        0x0 */
+            uint32_t reg_gpio_15_func_sel     : 5; /* [12: 8],        r/w,        0xb */
+            uint32_t reserved_13_15           : 3; /* [15:13],       rsvd,        0x0 */
+            uint32_t reg_gpio_15_int_mode_set : 4; /* [19:16],        r/w,        0x0 */
+            uint32_t reg_gpio_15_int_clr      : 1; /* [   20],        r/w,        0x0 */
+            uint32_t gpio_15_int_stat         : 1; /* [   21],          r,        0x0 */
+            uint32_t reg_gpio_15_int_mask     : 1; /* [   22],        r/w,        0x1 */
+            uint32_t reserved_23              : 1; /* [   23],       rsvd,        0x0 */
+            uint32_t reg_gpio_15_o            : 1; /* [   24],        r/w,        0x0 */
+            uint32_t reg_gpio_15_set          : 1; /* [   25],        w1p,        0x0 */
+            uint32_t reg_gpio_15_clr          : 1; /* [   26],        w1p,        0x0 */
+            uint32_t reserved_27              : 1; /* [   27],       rsvd,        0x0 */
+            uint32_t reg_gpio_15_i            : 1; /* [   28],          r,        0x0 */
+            uint32_t reserved_29              : 1; /* [   29],       rsvd,        0x0 */
+            uint32_t reg_gpio_15_mode         : 2; /* [31:30],        r/w,        0x0 */
+        } BF;
+        uint32_t WORD;
+    } gpio_cfg15;
 };
 
 typedef volatile struct glb_reg glb_reg_t;
