2a24022c287fd69e5dae7626176be770d64a6380
9names <60134748+9names@users.noreply.github.com>
2023-01-21 14:04:32 +1100

    Use xmlstarlet to format SVD

--- bl808-data/regen.sh
+++ bl808-data/regen.sh
@@ -2,5 +2,5 @@
 cargo build --bin svd_generator --release
 mkdir -p generated
 target/release/svd_generator > generated/BL808.svd
-#xmllint --format --nocompact bl808.svd > bl808_formatted.svd
-#xmllint --format --nocompact bl808_generated.svd > bl808_generated_formatted.svd
+xmlstarlet fo generated/BL808.svd > generated/BL808_formatted.svd
+mv generated/BL808_formatted.svd generated/BL808.svd
