f6a1157ed42627b388d6b8cac30510916c8274b1
9names <60134748+9names@users.noreply.github.com>
2023-01-24 21:59:40 +1100

    Add more test registers to glb_basic.h

--- bl808-data/sources/headers/glb_basic.h
+++ bl808-data/sources/headers/glb_basic.h
@@ -75,6 +75,54 @@ struct glb_reg {
         } BF;
         uint32_t WORD;
     } gpio_cfg15;
+
+    /* 0x60 : core_cfg20 */
+    union {
+        struct {
+            uint32_t np_int_clr0 : 32; /* [31: 0],        w1c,        0x0 */
+        } BF;
+        uint32_t WORD;
+    } core_cfg20;
+
+    /* 0x64 : core_cfg21 */
+    union {
+        struct {
+            uint32_t np_int_clr1                    : 32; /* [31: 0],        w1c,        0x0 */
+        }BF;
+        uint32_t WORD;
+    } core_cfg21;
+
+    /* 0x3C : Auto CMD12 Error Status Register */
+    union {
+        struct
+        {
+            uint16_t AUTO_CMD12_NOT_EXE   : 1; /* [    0],       RW1C,        0x0 */
+            uint16_t AUTO_CMD_TIMEOUT_ERR : 1; /* [    1],       RW1C,        0x0 */
+            uint16_t AUTO_CMD_CRC_ERR     : 1; /* [    2],       RW1C,        0x0 */
+            uint16_t AUTO_CMD_END_BIT_ERR : 1; /* [    3],       RW1C,        0x0 */
+            uint16_t AUTO_CMD_INDEX_ERR   : 1; /* [    4],       RW1C,        0x0 */
+            uint16_t Reserved_6_5         : 2; /* [ 6: 5],       RSVD,        0x0 */
+            uint16_t CMD_NOT_ISSUED       : 1; /* [    7],        ROC,        0x0 */
+            uint16_t Reserved_15_8        : 8; /* [15: 8],       RSVD,        0x0 */
+        } BF;
+        uint16_t SHORT;
+    } SD_AUTO_CMD12_ERROR_STATUS;
+
+    /* 0x3e : Host Control 2 Register */
+    union {
+        struct
+        {
+            uint16_t UHS_MODE_SEL     : 3; /* [ 2: 0],         RW,        0x0 */
+            uint16_t SDH_V18_EN       : 1; /* [    3],         RW,        0x0 */
+            uint16_t DRV_STRENGTH_SEL : 2; /* [ 5: 4],         RW,        0x0 */
+            uint16_t EXE_TUNING       : 1; /* [    6],       RWAC,        0x0 */
+            uint16_t SAMPLING_CLK_SEL : 1; /* [    7],         RW,        0x0 */
+            uint16_t Reserved_13_8    : 6; /* [13: 8],       RSVD,        0x0 */
+            uint16_t ASYNC_INT_EN     : 1; /* [   14],         RW,        0x1 */
+            uint16_t PRE_VAL_EN       : 1; /* [   15],         RW,        0x0 */
+        } BF;
+        uint16_t SHORT;
+    } HOST_CTRL_2;
 };
 
 typedef volatile struct glb_reg glb_reg_t;
