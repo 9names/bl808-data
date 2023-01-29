ea2d7dfeedf8fc5168247312a75bc92254fedc52
9names <60134748+9names@users.noreply.github.com>
2023-01-29 15:53:52 +1100

    Add yaml_generator to regen scripts

--- bl808-data/regen.bat
+++ bl808-data/regen.bat
@@ -13,3 +13,6 @@ svdtools convert generated/BL808.svd generated/BL808.yaml
 svdtools convert generated/BL808.yaml generated/BL808.svd
 @REM Also make a memory map file for quick comparisons
 svdtools mmap generated/BL808.svd > generated/BL808.mmap
+
+@REM Generate yaml files directly too
+cargo run --release --bin yaml_generator
--- bl808-data/regen.sh
+++ bl808-data/regen.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-cargo build --bin svd_generator --release
 mkdir -p generated
-target/release/svd_generator > generated/BL808.svd
+cargo run --release --bin svd_generator > generated/BL808.svd
 # Convert the SVD to yaml to make it a little easier for humans to read
 svdtools convert generated/BL808.svd generated/BL808.yaml
 # Convert it back to SVD again, so our shoddily formatted SVD is a bit nicer
 svdtools convert generated/BL808.yaml generated/BL808.svd
+# Also make a memory map file for quick comparisons
+svdtools mmap generated/BL808.svd > generated/BL808.mmap
+
+# Generate yaml files directly too
+cargo run --release --bin yaml_generator
